6aed3c7a2c8ff8fd82d01116890d580c0d4127c3
Jarom Denny <jaromdenny@Jaroms-MacBook-Pro.local>
2018-03-26 07:34:35 +1100

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//keys.js - determine production or dev environment for key
+
+if (process.env.NODE_ENV === 'production') {
+	//in Prod
+	module.exports = require dev('./prod');
+} else {
+	//in Dev
+	module.exports = require dev('./dev');
+}
---
+++ routes/authRoutes.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+
+module.exports = app => {
+	app.get('/', (req, res) => {
+		res.send(
+			'<!DOCTYPE html><head></head><body><h1 style="color:blue;font-size:4em;">To Authenticate via Google</h1><br><a href="/auth/google">google<a></body><html>'
+		);
+	});
+
+	app.get(
+		'/auth/google',
+		passport.authenticate('google', {
+			scope: ['profile', 'email']
+		})
+	);
+
+	app.get('/auth/google/callback', passport.authenticate('google'));
+
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	});
+};
