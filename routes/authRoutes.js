7a4838044f7ef96c19ce02918a907ed5e712a38e
Jarom Denny <jaromdenny@Jaroms-MacBook-Pro.local>
2018-04-10 07:34:24 +1000

    removed server base route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,6 @@
 const passport = require('passport');
 
 module.exports = app => {
-	app.get('/', (req, res) => {
-		res.send(
-			'<!DOCTYPE html><head></head><body><h1 style="color:blue;font-size:4em;">To Authenticate via Google</h1><br><a href="/auth/google">google<a></body><html>'
-		);
-	});
-
 	app.get(
 		'/auth/google',
 		passport.authenticate('google', {
